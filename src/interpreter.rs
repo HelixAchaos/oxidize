ad315fa973380d8f86b1dbf259f4e5e4ed704a76
Helix Achaos <peter.e.chon@gmail.com>
2023-02-21 07:00:50 -0500

    meh deref, but everything else is good

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,91 +2,113 @@ use std::{collections::HashMap, hash::Hash};
 
 use crate::ast::Expr;
 
-
-type Var = String;
 type Value = i64;
-type Address = i64;
-type ColoredValue = (bool, Value);
-type Memory = HashMap<Address, ColoredValue>; // true -> pointed memory can be modified
-type Context = HashMap<Var, ColoredValue>; // true -> variable name can be reassigned to
+type ColoredValue = (bool, Value); // false -> pointed memory cannot be modified
+type Memory = HashMap<Address, ColoredValue>; // Delta
+type Var = String;
+type Address = Value;
+type ColoredAddress = (bool, Address);
+type Context = HashMap<Var, ColoredAddress>; // Mu
 
-// fn assign_var (context: Vec<Context>, val: Value) -> Result<unit, String> {
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-// }
-
-fn get_var (vars: &Vec<Context>, name: &Var) -> Result<ColoredValue, String> {
-    for scope in vars.iter().rev() {
-        if let Some(val) = scope.get(name) {
-            return Ok(*val);
-        }
-    }
-    return Err(format!("Failed to find name {} in context", name))
+fn generate_address() -> Address {
+    static COUNTER: AtomicUsize = AtomicUsize::new(0);
+    let result = COUNTER.fetch_add(1, Ordering::Relaxed);
+    result as i64
 }
 
-// fn assign_var (vars: Vec<Context>, name: &Var, val: Value) -> Result<Vec<Context>, String> {
-
-// }
-
-pub fn eval<'a>(expr: &'a Expr, vars: &mut Vec<Context>, memory: &mut Memory) -> Result<ColoredValue, String> {
+pub fn eval<'a>(
+    expr: &'a Expr,
+    vars: &mut Vec<Context>,
+    memory: &mut Memory,
+) -> Result<ColoredValue, String> {
     match expr {
-        Expr::Num(x) => Ok((false, *x)),
-        Expr::Neg(a) => Ok((false, -eval(a, vars, memory)?.1)),
-        Expr::Add(a, b) => Ok((false, eval(a, vars, memory)?.1 + eval(b, vars, memory)?.1)),
-        Expr::Sub(a, b) => Ok((false, eval(a, vars, memory)?.1 - eval(b, vars, memory)?.1)),
-        Expr::Mul(a, b) => Ok((false, eval(a, vars, memory)?.1 * eval(b, vars, memory)?.1)),
-        Expr::Div(a, b) => Ok((false, eval(a, vars, memory)?.1 / eval(b, vars, memory)?.1)),
-        Expr::Var(name) => get_var(vars, name),
+        Expr::Num(x) => Ok((true, x.to_owned())),
+        Expr::Neg(a) => Ok((true, -eval(a, vars, memory)?.1)),
+        Expr::Add(a, b) => Ok((true, eval(a, vars, memory)?.1 + eval(b, vars, memory)?.1)),
+        Expr::Sub(a, b) => Ok((true, eval(a, vars, memory)?.1 - eval(b, vars, memory)?.1)),
+        Expr::Mul(a, b) => Ok((true, eval(a, vars, memory)?.1 * eval(b, vars, memory)?.1)),
+        Expr::Div(a, b) => Ok((true, eval(a, vars, memory)?.1 / eval(b, vars, memory)?.1)),
+        Expr::Var(name) => {
+            if let Some((is_mut, ell)) = vars.into_iter().rev().find_map(|scope| scope.get(name)) {
+                if let Some((_, val)) = memory.get(ell) {
+                    Ok((is_mut.to_owned(), val.to_owned()))
+                } else {
+                    Err(format!(
+                        "Attempted to access an address that cannot be found"
+                    ))
+                }
+            } else {
+                Err(format!("Failed to find name {} in context", name))
+            }
+        }
         Expr::Seq(e1, e2) => {
             eval(e1, vars, memory)?;
             Ok(eval(e2, vars, memory)?)
         }
-        Expr::Ref(_e) => {
-            todo!("How to make this work? Should I store the reference in the value? That would make the values much fatter tho.")
-        },
-        Expr::MutRef(_e) => {
-            todo!("same code pretty much as expr::ref")
-        }, // (true,) <- memory
+        Expr::Ref(e) => {
+            let (_, val) = eval(e, vars, memory)?;
+            let ell = generate_address();
+            memory.insert(ell, (false, val));
+            Ok((false, ell))
+        }
+        Expr::MutRef(e) => {
+            let (is_mut, val) = eval(e, vars, memory)?;
+            if !is_mut {
+                Err(format!(
+                    "Cannot borrow expression {:?} as mutable, as it's not declared as mutable'",
+                    e
+                ))?
+            }
+            let ell = generate_address();
+            memory.insert(ell, (true, val));
+            Ok((true, ell))
+        } // (true,) <- memory
         Expr::DeRef(e) => {
             let (_, address) = eval(e, vars, memory)?;
             if let Some(value) = memory.get(&address) {
                 Ok(*value)
             } else {
-                Err(format!("Attempted to dereference a pointer which points to some unkown value"))
+                Err(format!(
+                    "Attempted to dereference a pointer which points to some unkown value"
+                ))
             }
-        },
+        }
         Expr::Let { name, rhs, then } => {
-            let (_, val) = eval(rhs, vars, memory)?;
-            let is_mut = vars.iter().rev().all(|scope| if let Some((is_mut, _)) = scope.get(name) { *is_mut } else { true });
-            let is_found = vars.iter().any(|scope| scope.get(name).is_some());
-            if !is_mut && is_found {
-                Err(format!("Attempted to resassign to immutable binding"))?
+            if vars.into_iter().any(|scope| scope.contains_key(name)) {
+                Err(format!("Attempted to bind to already bound name: {}", name))?
             }
 
+            let (_, val) = eval(rhs, vars, memory)?;
+            let ell = generate_address();
+            memory.insert(ell, (false, val));
+
             vars.push(
-                [(name.clone(), (false, val))]
-                .iter()
-                .cloned()
-                .collect::<HashMap<_, _>>()
+                [(name.clone(), (false, ell))]
+                    .iter()
+                    .cloned()
+                    .collect::<HashMap<_, _>>(),
             );
-            
             let output = eval(then, vars, memory);
             vars.pop();
             output
         }
         Expr::MutLet { name, rhs, then } => {
-            let (_, val) = eval(rhs, vars, memory)?;
-            let is_mut = vars.iter().rev().all(|scope| if let Some((is_mut, _)) = scope.get(name) { *is_mut } else { true });
-            if !is_mut {
-                Err(format!("Attempted to make mutable binding on an immutably bound variable"))?
+            if vars.into_iter().any(|scope| scope.contains_key(name)) {
+                Err(format!("Attempted to bind to already bound name: {}", name))?
             }
 
+            let (_, val) = eval(rhs, vars, memory)?;
+            let ell = generate_address();
+            memory.insert(ell, (true, val));
+
             vars.push(
-                [(name.clone(), (true, val))]
-                .iter()
-                .cloned()
-                .collect::<HashMap<_, _>>()
+                [(name.clone(), (true, ell))]
+                    .iter()
+                    .cloned()
+                    .collect::<HashMap<_, _>>(),
             );
-            
             let output = eval(then, vars, memory);
             vars.pop();
             output
@@ -96,39 +118,24 @@ pub fn eval<'a>(expr: &'a Expr, vars: &mut Vec<Context>, memory: &mut Memory) ->
             name,
             rhs,
         } => {
-
-            // type check to see if the rhs has gte ref/mutrefs than deref_counts
-            let mut ref_mut_ref_count = 0;
-            while true {
-                match **rhs {
-                    Expr::Ref(_) | Expr::MutRef(_) => {
-                        ref_mut_ref_count += 1;
-                    }
-                    _ => {
-                        break;
-                    }
-                };
-            }
-            if ref_mut_ref_count < *deref_count {
-                Err(format!("You can only assign valid references for, well, references"))?
-            }
-
-            let (is_mut, mut addr) = get_var(vars, name)?;
-            if !is_mut {
-                Err(format!("Attempted to assign to the pointed value of an immutably bound reference"))?
-            }
-            while *deref_count > 0 {
-                if let Some((is_mut, a)) = memory.get(&addr) {
-                    if !is_mut {
-                        Err(format!("Attempted to assign to the pointed value of an immutably bound reference"))?
-                    }
-                    addr = *a;
-                }
-            }
-
             let val = eval(rhs, vars, memory)?;
-            memory.insert(addr, val);
-
+            if let Some((_, mut ell)) = vars.iter().rev().find_map(|scope| scope.get(name)) {
+                while *deref_count > 0 {
+                    if let Some((c, addr)) = memory.get(&ell) {
+                        if !c {
+                            Err(format!(
+                                "Attempted to assign to the pointed value of an immutably bound reference"
+                            ))?
+                        }
+                        ell = *addr;
+                    } else {
+                        Err(format!(""))?
+                    }
+                }
+                memory.insert(ell, val);
+            } else {
+                Err(format!("Attempted to assign to an unbound name {}", name))?
+            }
             Ok(val)
         }
     }
